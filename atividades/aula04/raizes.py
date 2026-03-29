ab60936ae96110509160e45eaea28881e53e170c
Luiz Henrique <luiz.teixeira2803@gmail.com>
2026-03-29 13:39:39 -0300

    .

---
+++ atividades/aula04/raizes.py
@@ -0,0 +1,9 @@
+a = float(input("Digite o valor de a: "))
+b = float(input("Digite o valor de b: "))
+c = float(input("Digite o valor de c: "))
+
+x1 = (-b + b**2 - 4 * a * c) / (2*a)
+x2 = (-b - b**2 - 4 * a * c) / (2*a)
+
+print("O valor de x1 é: ", x1)
+print("O valor de x2 é: ", x2)
