d227b8e94f8acb134b3a38f32e6db2683243ccf7
Luiz Henrique <luiz.teixeira2803@gmail.com>
2026-03-30 21:04:35 -0300

    .

--- atividades/aula04/areas.py
+++ atividades/aula04/areas.py
@@ -1,4 +1,8 @@
+print("Esse código faz o cálculo da área de um triângulo")
+
 altura = float(input("Digite a altura: "))
 base = float(input("Digite a base: "))
+
 area = (base * altura) / 2
+
 print("A área do triângulo é: ", area)
