b2570461a9a8517f0367f38c05512ef27754eff6
Luiz Henrique <luiz.teixeira2803@gmail.com>
2026-04-08 22:30:26 -0300

    entendi quase nada
    Added a program to calculate change and print the number of banknotes and coins.

--- atividades/atividade02/problema-troco.py
+++ atividades/atividade02/problema-troco.py
@@ -1,1 +1,20 @@
+print("esse programa calcula as cédulas de um troco")
 
+valor_pago = int(input("Qual o valor pago?: "))
+valor_conta = int(input("Qual o valor da conta?: "))
+
+troco = valor_pago - valor_conta
+print("O valor do troco é: ", troco)
+
+ced200 = troco // 200
+print("#ced200: ", 200)
+troco = troco % 200
+
+ced200 = troco // 100
+print("#ced100: ", 100)
+troco = troco % 100
+
+ced200 = troco // 2
+print("#ced2: ", 2)
+troco = troco % 2
+print("#moedas1: ", troco)
