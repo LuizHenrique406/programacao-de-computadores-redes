145f0d83e661ca6ea241e6c034c8086db6016c68
Luiz Henrique <luiz.teixeira2803@gmail.com>
2026-04-08 22:36:29 -0300

    dá pra entender um pouco, é de boa até

---
+++ atividades/atividade02/problema-troco-resolucao1.py
@@ -0,0 +1,12 @@
+num = int(input("Número: ")
+					
+dig1 = num // 1000
+num = num % 1000
+dig2 = num // 100
+num = num % 100
+dig3 = num // 10
+num = num % 10
+dig4 = num % 10
+soma = dig1 + dig2 + dig3 + dig4
+
+print("soma: ", soma)
