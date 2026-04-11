c6e0868024cc98c8e64c51e8fc26ad1816f16236
Luiz Henrique <luiz.teixeira2803@gmail.com>
2026-04-11 18:16:27 -0300

    comando condicional

---
+++ atividades/atividade03/if_else-ordem-crescente.py
@@ -0,0 +1,8 @@
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite ouro número: "))
+
+if num1 < num2:
+    print("Os números são: ", num1, num2)
+else:
+    print("Os números são: ", num2, num1)
+    
