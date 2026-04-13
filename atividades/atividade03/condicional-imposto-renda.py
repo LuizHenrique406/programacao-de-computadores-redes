f0d8b2daa486948855110507487b80b2bf63a63b
Luiz Henrique <luiz.teixeira2803@gmail.com>
2026-04-13 20:55:29 -0300

    melhorando

--- atividades/atividade03/condicional-imposto-renda.py
+++ atividades/atividade03/condicional-imposto-renda.py
@@ -7,13 +7,13 @@ if renda < 2.428.80:
 else:
     if renda <= 2.826.65:
         imposto_renda1 = renda * 7.5 / 100 - 182.16
-            print("Você irá precisar pagar: "), imposto_renda1
+        print("Você irá precisar pagar: "), imposto_renda1)
             else:
-                 imposto_renda2 = renda * 15 / 100 - 394.16
-                     print("Você irá precisar pagar: "), imposto_renda2
-                        if renda <= 4.664.68:
+                imposto_renda2 = renda * 15 / 100 - 394.16
+                print("Você irá precisar pagar: "), imposto_renda2)
+                      if renda <= 4.664.68:
                             imoposto_renda3 = renda * 22.5 / 100 - 675.49
-                                print("Você irá precisar pagar: "), imposto_renda3
-                                    else renda > 4.664.68:
-                                        imposto_renda4 = renda * 27.5 / 100 - 908,73
-                                            print("Você irá precisar pagar: "), imposto_renda4
+                            print("Você irá precisar pagar: "), imposto_renda3)
+                                else renda > 4.664.68:
+                                    imposto_renda4 = renda * 27.5 / 100 - 908,73
+                                    print("Você irá precisar pagar: "), imposto_renda4)
