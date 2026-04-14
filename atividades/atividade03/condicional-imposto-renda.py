e978792593eeff7aad945dc213e10be35536abb7
Luiz Henrique <luiz.teixeira2803@gmail.com>
2026-04-13 21:11:55 -0300

    ajustado

--- atividades/atividade03/condicional-imposto-renda.py
+++ atividades/atividade03/condicional-imposto-renda.py
@@ -2,17 +2,17 @@ print("Esse programa calcula o seu imposto de renda")
 
 renda = float(input("Coloque o seu salário mensal: "))
 
-if renda < 2.428.80 :
-    pritn("Não precisa pagar imposto de renda")
-elif renda <= 2.826.65:
-  imposto_renda1 = renda * 7.5 / 100 - 182.16
-  print("Você irá precisar pagar: "), imposto_renda1)
-elif renda <= 3.751.05:
-  imposto_renda2 = renda * 15 / 100 - 394.16
-  print("Você irá precisar pagar: "), imposto_renda2)
-elif renda <= 4.664.68:
-imoposto_renda3 = renda * 22.5 / 100 - 675.49
-print("Você irá precisar pagar: "), imposto_renda3)
-  else:  
+if renda < 2428.80 :
+    print("Não precisa pagar imposto de renda")
+elif renda <= 2826.65:
+    imposto_renda1 = renda * 7.5 / 100 - 182.16
+    print("Você irá precisar pagar: ", imposto_renda1)
+elif renda <= 3751.05:
+    imposto_renda2 = renda * 15 / 100 - 394.16
+    print("Você irá precisar pagar: ", imposto_renda2)
+elif renda <= 4664.68:
+    imposto_renda3 = renda * 22.5 / 100 - 675.49
+    print("Você irá precisar pagar: ", imposto_renda3)
+else:
     imposto_renda4 = renda * 27.5 / 100 - 908,73
-    print("Você irá precisar pagar: "), imposto_renda4)
+    print("Você irá precisar pagar: ", imposto_renda4)
