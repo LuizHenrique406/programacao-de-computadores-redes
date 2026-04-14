5125884e064b106ee2d7def5485173f020e81243
Luiz Henrique <luiz.teixeira2803@gmail.com>
2026-04-13 22:03:53 -0300

    ajustado novamente

--- atividades/atividade03/calculo-IMC-condicional.py
+++ atividades/atividade03/calculo-IMC-condicional.py
@@ -9,13 +9,13 @@ print("O seu IMC é: ", imc)
 if imc < 18.5:
     print("MAGRESA")
 else:
-  if imc <= 24.9:
-    print("NORMAL")
-  else:
-    print("SOBREPESO")
-if imc <= 34.9:
-    print("OBESIDADE GRAU I")
-else:
-   print("OBESIDADE GRAU II")
-if imc > 40:
-    print("OBESIDADE III")
+    if imc <= 24.9:
+        print("NORMAL")
+    else:
+        print("SOBREPESO")
+        if imc <= 34.9:
+            print("OBESIDADE GRAU I")
+        else:
+            print("OBESIDADE GRAU II")
+            if imc > 40:
+                print("OBESIDADE III")
