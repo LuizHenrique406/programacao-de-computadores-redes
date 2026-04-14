d950045c871768d9a987bf0a3fcf049ca2dde972
Luiz Henrique <luiz.teixeira2803@gmail.com>
2026-04-13 21:16:46 -0300

    ajustado também

--- atividades/atividade03/calculo-IMC-condicional.py
+++ atividades/atividade03/calculo-IMC-condicional.py
@@ -1,4 +1,5 @@
 print("Esse programa calcula o Índice de Massa Corporal")
+
 peso = float(input("Coloque o seu peso: "))
 altura = float(input("Coloque a sua altura: "))
 
@@ -8,13 +9,13 @@ print("O seu IMC é: ", imc)
 if imc < 18.5:
     print("MAGRESA")
 else:
-    if imc <= 24.9:
-        print("NORMAL")
-        else:
-            print("SOBREPESO")
-            if imc <= 34.9:
-                print("OBESIDADE GRAU I")
-                else:
-                    print("OBESIDADE GRAU II")
-                        if imc > 40:
-                            print("OBESIDADE III")
+  if imc <= 24.9:
+    print("NORMAL")
+  else:
+    print("SOBREPESO")
+if imc <= 34.9:
+    print("OBESIDADE GRAU I")
+else:
+   print("OBESIDADE GRAU II")
+if imc > 40:
+    print("OBESIDADE III")
