cb4595c2f5ec34a5fb4d8d68638105c8645fc2a6
Luiz Henrique <luiz.teixeira2803@gmail.com>
2026-04-17 11:14:50 -0300

    feito e aprendido (if + else)

--- atividades/atividade03/condicional-imposto-renda.py
+++ atividades/atividade03/condicional-imposto-renda.py
@@ -1,18 +1,21 @@
-print("Esse programa calcula o seu imposto de renda")
+print("Esse programa calcula o seu Imposto de R6000enda")
 
-renda = float(input("Digite o seu salário mensal: "))
+renda = float(input("Digite o seu sálario mensal: "))
 
 if renda < 2428.80:
-    print("Não precisa pagar imposto de renda")
-elif renda <= 2826.65:
-    imposto_renda1 = renda * 7.5 / 100 - 182.16
-    print("Você irá precisar pagar: ", imposto_renda1)
-elif renda <= 3751.05:
-    imposto_renda2 = renda * 15 / 100 - 394.16
-    print("Você irá precisar pagar: ", imposto_renda2)
-elif renda <= 4664.68:
-    imposto_renda3 = renda * 22.5 / 100 - 675.49
-    print("Você irá precisar pagar: ", imposto_renda3)
+    print("ISENTO")
 else:
-    imposto_renda4 = renda * 27.5 / 100 - 908.73
-    print("Você irá precisar pagar: ", imposto_renda4)
+    if renda <= 2826.65:
+        imposto = renda * 7.5 / 100 - 182.16
+        print("Você irá pagar: ", imposto)
+    else:
+       if renda <= 3751.05:
+            imposto = renda * 15/ 100 - 394.16
+            print("Você irá pagar: ", imposto)
+       else:
+          if renda <= 4664.68:
+            imposto = renda * 22.5 / 100 - 675.49
+            print("Você irá precisar pagar: ", imposto)
+          else:
+            imposto = renda * 27.5 / 100 - 908.73
+            print("Você irá precisar pagar: ", imposto)
