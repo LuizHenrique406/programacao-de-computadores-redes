38d159e4516ade891f17dffaf1cd85e7aa74c001
Luiz Henrique <luiz.teixeira2803@gmail.com>
2026-04-21 15:05:49 -0300

    ajuste feito, mas faltando uma condição da questão

--- atividades/lista-03/questao-03.py
+++ atividades/lista-03/questao-03.py
@@ -5,34 +5,29 @@ horas1 = int(input("Digite a hora da sua partida: "))
 minutos1 = int(input("Digite o minutos da sua partida: "))
 horas2 = int(input("Digite a hora da sua chegada: "))
 minutos2 = int(input("Digite o minuto da sua chegada: "))
-print("Digite sim ou não")
-viagem_dias = str(input("Sua viagem foi no mesmo dia?: "))
-if viagem_dias == "não":
-    dias_viagem = int(input("Há quantos dias você estar viajando?: "))
-    dias_viagem = dias_viagem * (24 * 60)
+total_minutos1 = horas1 * 60 + minutos1
+total_minutos2 = horas2 * 60 + minutos2
 
-else:
-    total_minutos1 = horas1 * 60 + minutos1
-    total_minutos2 = horas2 * 60 + minutos2
-    if total_minutos2 > total_minutos1:
-        print()
-        print("Pausas, almoço e etc, em MINUTOS")
-        hora_descanso = int(input("Tempo total que seu carro ficou parado: "))
-        print()
-        combustivel_gasto = float(input("Quantidade total de combustíel gasto em litros: "))
-        print()
-        print("Valor em R$")
-        preco_litro = float(input("Qual foi o valor em R$ do litro de combustível gasto?: "))
-        print()
-        print("Distância em Km")
-        distancia_percorrida = int(input("Qual a distância total percorrida da sua viagem?: "))
+if total_minutos2 > total_minutos1:
+    print()
+    print("Pausas, almoço e etc, em MINUTOS")
+    hora_descanso = int(input("Tempo total que seu carro ficou parado: "))
+    print()
+    combustivel_gasto = float(input("Quantidade total de combustíel gasto em litros: "))
+    print()
+    print("Valor em R$")
+    preco_litro = float(input("Qual foi o valor em R$ do litro de combustível gasto?: "))
+    print()
+    print("Distância em Km")
+    distancia_percorrida = int(input("Qual a distância total percorrida da sua viagem?: "))
 
-        total_geral = total_minutos2 - total_minutos1 + dias_viagem
+    total_geral = total_minutos2 - total_minutos1 + hora_descanso
 
-        horas_final = total_geral // 60
-        minutos_final = total_geral % 60
-        print("O tempo tempo total da viagem foi de", horas_final, "horas e", minutos_final, "minutos")
-
+    horas_final = total_geral // 60
+    minutos_final = total_geral % 60
+    print("O tempo tempo total da viagem foi de", horas_final, "horas e", minutos_final, "minutos")
+    if horas_final < 24:
+        
         vm = distancia_percorrida / (total_geral / 60)
         print("A velocidade média global é", vm)
 
@@ -47,3 +42,5 @@ else:
 
         custo_total = (distancia_percorrida / desempenho_carro) * preco_litro
         print("O seu gasto total na viagem foi de", custo_total, "R$")
+else:
+    print("ERRO, O TEMPO DE CHEGADA É MENOR QUE O DE PARTIDA OU FORMATO DE HORA ERRADA")
