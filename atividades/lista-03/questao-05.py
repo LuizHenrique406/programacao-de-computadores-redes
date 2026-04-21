d29449059bd7aab3280b7e28a8e614d77462ec8b
Luiz Henrique <luiz.teixeira2803@gmail.com>
2026-04-21 08:54:25 -0300

    Questão apólice de seguro de um veículo, ajuste feito e ainda a ser analisada pelo professor

--- atividades/lista-03/questao-05.py
+++ atividades/lista-03/questao-05.py
@@ -10,23 +10,25 @@ if idade_condutor >= 18:
         if idade_condutor <= 60:
             print("Sem adicional")
         else:
-            valor_base = valor_carro * 0,10
+            valor_base = valor_carro * 0.10
 
-    print("Digite T para TRABALHO e L para LAZER")              
-    trabalho_lazer = str(input("Uso do veículo é para TRABALHO ou LAZER?: "))
-
-    if trabalho_lazer == "T":
-        valor_base + 300
+    print("Digite T para TRABALHO e L para LAZER")
+    uso_carro = str(input("Qual o uso do seu veículo?: "))
+    
+    if uso_carro == "T":
+        valor_base = valor_base + 300
     else:
-        valor_base - 100
+        valor_base = valor_base - 100
 
     acidentes_ano = int(input("Quantos acidentes você teve no último ano?: "))
 
     valor_base = (acidentes_ano * 300) + valor_base
+
     if valor_base < 7000:
         print(valor_base)
         print("Seu seguro pode ser contratado")
     else:
         print("Seu seguro está sob análise") 
+
 else:
     print("CONTRATAÇÃO REJEITADA")
