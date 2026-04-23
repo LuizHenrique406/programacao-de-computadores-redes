5c2cb31e2ae07733c2d5a4b23264560e345bf0b3
Luiz Henrique <luiz.teixeira2803@gmail.com>
2026-04-23 14:55:11 +0000

    questão 2 da primeira lista, ainda a terminar

---
+++ atividades/lista-01/questao-02.py
@@ -0,0 +1,8 @@
+print("Esse programa realiza o calculo de uma equação de 2° grau")
+a = int(input("Digite o valor de a: "))
+b = int(input("Digite o valor de b:  "))
+c = int(input("Digite o valor de c: "))
+
+delta = b**2 - 4 * a * c
+
+
