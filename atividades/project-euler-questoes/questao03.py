5b4fbdd7b645a5f5bf77b3b6dd4bd4aa9e8cb393
LuizHenrique406 <luiz.henriqueteixeira2008@gmail.com>
2026-04-29 18:41:22 -0300

    refeito

--- atividades/project-euler-questoes/questao03.py
+++ atividades/project-euler-questoes/questao03.py
@@ -1,8 +1,10 @@
-num = 84
 div = 2
-
+num = int(input("Digite um número: "))
+maior = 2
 while num > 1:
     if num % div == 0:
         num = num // div
+        maior = div
     else:
         div = div + 1
+print("O maior divisor foi", maior)
