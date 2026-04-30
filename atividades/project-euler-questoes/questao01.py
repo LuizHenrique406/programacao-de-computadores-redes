a506ba75f9eaa8bc60ac46db772ee285e704ef60
Luiz Henrique <luiz.teixeira2803@gmail.com>
2026-04-30 11:56:12 +0000

    terminar

---
+++ atividades/project-euler-questoes/questao01.py
@@ -0,0 +1,13 @@
+print("Esse programa calcula a soma dos múltiplos de 3 a 5")
+
+num1 = 3
+num2 = 5
+x = 1
+while x < 1001:
+    soma = num1 * x
+    x = x + 1
+    
+
+
+resultado = soma_total
+print("A soma dos múltiplos são", resultado)
