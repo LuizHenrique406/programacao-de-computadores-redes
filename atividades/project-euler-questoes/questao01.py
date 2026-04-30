d314d36bc530078746f8171ff2282758a3d555ed
Luiz Henrique <luiz.teixeira2803@gmail.com>
2026-04-30 13:12:18 +0000

    feito

--- atividades/project-euler-questoes/questao01.py
+++ atividades/project-euler-questoes/questao01.py
@@ -1,13 +1,15 @@
-print("Esse programa calcula a soma dos múltiplos de 3 a 5")
+print("Esse programa calcula a soma dos múltiplos de 3 e 5")
 
 num1 = 3
 num2 = 5
-x = 1
-while x < 1001:
-    soma = num1 * x
-    x = x + 1
-    
+x = 3
+soma_total = 0  
 
+while x < 1000:
+    if x % num1 == 0 or x % num2 == 0:
+        soma_total = x + soma_total
+        x = x + 1
+    else:
+        x = x + 1
 
-resultado = soma_total
-print("A soma dos múltiplos são", resultado)
+print("A soma dos múltiplos são", soma_total)
