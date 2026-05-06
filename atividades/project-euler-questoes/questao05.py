a093570e70986dfac377ff17673508fecc8352ea
LuizHenrique406 <luiz.henriqueteixeira2008@gmail.com>
2026-05-06 10:29:42 -0300

    nada feito, rever erros de lógica ainda

--- atividades/project-euler-questoes/questao05.py
+++ atividades/project-euler-questoes/questao05.py
@@ -1,6 +1,13 @@
 print("Esse programa calcula divisores uniformemete")
-num = int(input("Digite um valor: "))
-div = 1
+num = 2
+div = 2
 while div < 21:
     if num % div == 0:
         div = div + 1
+    else:
+        num = num + 1
+
+if div == 20:
+    print("o número dividido uniformemente por 20 foi", num)
+else:
+    print("nada encontrado")
