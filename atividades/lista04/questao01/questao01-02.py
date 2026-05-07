e7206804faf6223f46fba468e3a2f6fb05cfb467
LuizHenrique406 <luiz.henriqueteixeira2008@gmail.com>
2026-05-06 21:36:26 -0300

    depois da IA, ela deu uma melhorada, mas deu pra entender perfeitamente

---
+++ atividades/lista04/questao01/questao01-02.py
@@ -0,0 +1,23 @@
+print("Esse programa acha o número primo na posição 137")
+
+div = 1
+num = 2
+contagem_num = 0
+ndiv = 0
+
+while contagem_num < 137:
+    div = 1
+    ndiv = 0
+    
+    while div <= num:
+        if num % div == 0:
+            ndiv = ndiv + 1
+        div = div + 1
+    
+    if ndiv == 2:
+        contagem_num = contagem_num + 1
+
+    num = num + 1
+    
+if contagem_num == 137:
+        print("O número primo da posição 137 é", num - 1)
