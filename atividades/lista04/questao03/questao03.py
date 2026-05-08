f30632f947444be2b835887cd8070f5e2cedcd3e
LuizHenrique406 <luiz.teixeira2803@gmail.com>
2026-05-08 15:17:18 -0300

    finalizado (sem ajuda de IA :)

--- atividades/lista04/questao03/questao03.py
+++ atividades/lista04/questao03/questao03.py
@@ -4,7 +4,7 @@
 # se ele for ímpar, multiplica por 3 e soma + 1
 # daí pega o resultado da primeira verificação e faz tudo de novo
 # toda vez que ele for dividindo, aumenta + 1 na contagem de termos
-# e no quinto termo, se tiver, quarda esse valor e imprime ele no finalo do código
+# e no quinto termo, se tiver, guarda esse valor e imprime ele no final do código
 # par = num / 2
 # impar = 3 * num + 1
 
@@ -13,23 +13,21 @@ print("Esse programa calcula os números de Collatz")
 
 num = int(input("Digite um número inteiro: "))
 
-veri_termos = 0
+veri_termos = 1
 termo5 = 0
 
 while num >= 2:
 
     if num % 2 == 0:
-        num = num / 2
+        num = num // 2
         veri_termos = veri_termos + 1
         print(num)
     else:
         num = 3 * num + 1
         veri_termos = veri_termos + 1
         print(num)
-    if veri_termos == 4:
+
+    if veri_termos == 5:
         termo5 = num
 
-if veri_termos == 5:    
-    print("Esse número tem", veri_termos, "de sequência", "e seu 5° termo é", termo5)
-else:
-    print("Esse número tem", veri_termos, "de sequência")
+print("Esse número tem", veri_termos, "de sequência", "e seu 5° termo é", termo5)
