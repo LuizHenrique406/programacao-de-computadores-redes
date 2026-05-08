89679e7274ee6d02e386ce7e327b23bd05b376a8
LuizHenrique046 <luiz.teixeira2803@gmail.com>
2026-05-08 10:16:18 -0300

    feito, IA ajudou um pouco na lógica e nos erros de estrutura

--- atividades/lista04/questao02/questao02-01.py
+++ atividades/lista04/questao02/questao02-01.py
@@ -1,18 +1,23 @@
 print("Esse programa calcula quantos políndromos tem entre 10 e 1000000")
 
-contagem_poli = 1
+contagem_poli = 0
+num1 = 10
+num2 = num1
+num_invertido = 0
 
-while contagem_poli < 1000:
-    num = 11
-    num_invertido = 0
+while num1 < 1000001:
+    num2 = num1
+    while num2 > 0:
+        num_invertido = num_invertido * 10 + (num2 % 10)
+        num2 = num2 // 10
 
-    while num > 0:
-        num_invertido = num_invertido * 10 + (num % 10)
-        num = num // 10
-
-    if num_invertido == num:
-        num = num + 1
-
-    contagem_poli = contagem_poli + 1
+    if num_invertido == num1:
+        num_invertido = 0
+        num1 = num1 + 1
+        contagem_poli = contagem_poli + 1
+    else:
+        num_invertido = 0
+        num1 = num1 + 1
+    
     
 print("Existem", contagem_poli, "políndromos entre 10 e 1000000")
