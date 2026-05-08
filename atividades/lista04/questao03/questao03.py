5e6f5478d86bc573e3d45bac40e76cab90152b13
LuizHenrique406 <luiz.teixeira2803@gmail.com>
2026-05-08 15:20:01 -0300

    finalizado (sem ajuda de IA :)

--- atividades/lista04/questao03/questao03.py
+++ atividades/lista04/questao03/questao03.py
@@ -1,14 +1,3 @@
-# receber um número
-# depois verificar se ele é ímpar ou par
-# se ele for par, divide por 2
-# se ele for ímpar, multiplica por 3 e soma + 1
-# daí pega o resultado da primeira verificação e faz tudo de novo
-# toda vez que ele for dividindo, aumenta + 1 na contagem de termos
-# e no quinto termo, se tiver, guarda esse valor e imprime ele no final do código
-# par = num / 2
-# impar = 3 * num + 1
-
-
 print("Esse programa calcula os números de Collatz")
 
 num = int(input("Digite um número inteiro: "))
