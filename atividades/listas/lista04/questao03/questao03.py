71ebdc709b0b102cff150204724136f145503efd
LuizHenrique406 <luiz.henriqueteixeira2008@gmail.com>
2026-05-09 11:59:53 -0300

    feito e corrigido com sucesso

--- atividades/listas/lista04/questao03/questao03.py
+++ atividades/listas/lista04/questao03/questao03.py
@@ -16,8 +16,8 @@ while num >= 2:
 
     if veri_termos == 5:
         termo5 = num
-        
-if termo5 == True:
+
+if veri_termos >= 5:
     print("Esse número tem", veri_termos, "de sequência", "e seu 5° termo é", termo5)
 else:
     print("Esse número tem", veri_termos, "de sequência")
