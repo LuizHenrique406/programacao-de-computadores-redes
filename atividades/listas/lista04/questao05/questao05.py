882d692a7f484229ab30ac4852aa8ab2b7345450
LuizHenrique406 <luiz.henriqueteixeira2008@gmail.com>
2026-05-09 14:47:11 -0300

    só colocando a ideia e listando objetivos. a fazer

---
+++ atividades/listas/lista04/questao05/questao05.py
@@ -0,0 +1,6 @@
+# perguntar ao usuário qual será a posição inicial do robô
+# depois perguntar uma string, que é com ela que o robô vai andar
+# depois definir as string com seus respectivos movimentos
+# depois fazer o plano cartesiano de alguma forma aí
+# no final, indicar a posição final do robô e quantos movimentos válidos ele fez
+# validar letras maiúsculas e minúsculas, e ignorar aquelas que não forem estabelecidas
