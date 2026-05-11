94a5515f0a645cee02505d1dc23eaf836d18d071
Luiz Henrique <luiz.teixeira2803@gmail.com>
2026-05-11 15:51:31 -0300

    em aula

---
+++ materiais-de-aula/forca/forca-random.py
@@ -0,0 +1,13 @@
+import random 
+
+print("Seu objetivo é acertar a palavra sorteada")
+palavras = [ "ARARA", "TURMA", "LIMÃO", "ABACAXI"]
+sorteada = palavras[    random.randint(0,3)]
+digitada = "_" * len(sorteada)
+
+while digitada != sorteada:
+    print(digitada)
+    letra = input("Digite uma letra: ")
+    for pos in range(len(sorteada)):
+        if sorteada[pos] == letra:
+            digitada = digitada[:pos] + letra + digitada[pos+1]
