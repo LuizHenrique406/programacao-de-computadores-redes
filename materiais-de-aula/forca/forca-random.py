44d814501800cda20c6f5cbf1ccbd22c59176245
LuizHenrique406 <luiz.teixeira2803@gmail.com>
2026-05-12 11:05:16 -0300

    adicionado algumas mudanças e implementado limite de tentativas

--- materiais-de-aula/forca/forca-random.py
+++ materiais-de-aula/forca/forca-random.py
@@ -5,9 +5,23 @@ palavras = [ "ARARA", "TURMA", "LIMÃO", "ABACAXI"]
 sorteada = palavras[random.randint(0,3)]
 digitada = "_" * len(sorteada)
 
-while digitada != sorteada:
+tentativas = 1
+
+while tentativas < 5:
+
     print(digitada)
     letra = input("Digite uma letra: ")
+    
     for pos in range(len(sorteada)):
         if sorteada[pos] == letra:
-            digitada = digitada[:pos] + letra + digitada[pos+1]
+            digitada = digitada[:pos] + letra + digitada[pos+1:]
+
+    if sorteada == digitada:
+        break
+    
+    tentativas = tentativas + 1
+
+if digitada == sorteada:
+    print("Acertou!!!")
+else:
+    print("Errou!!! a palavra era", sorteada)
