61224e1d2a37fd4e991216769105d2dd120ddef9
LuizHenrique406 <luiz.henriqueteixeira2008@gmail.com>
2026-05-18 10:14:11 -0300

    cálculo feito e finalizado

--- atividades/exercícios/exercícios05/cpf-validacao.py
+++ atividades/exercícios/exercícios05/cpf-validacao.py
@@ -1,14 +1,29 @@
-cpf = int(input("Digite seu CFP: "))
-soma = 0
+cpf1 = int(input("Digite seu CFP: "))
+soma1 = 0
+soma2 = 0
+cpf2 = cpf1
 for m in range(2,11):
-    n = cpf % 10
-    soma = soma + (n * m)
-    cpf = cpf // 10
+    n1 = cpf1 % 10
+    soma1 = soma1 + (n1 * m)
+    cpf1 = cpf1 // 10
 
-dv1 = soma % 11
+dv1 = soma1 % 11
 if dv1 >= 2:
     dv1 = 11 - dv1
 else:
     dv1 = 0
 
-print(dv1)
+for i in range(2,11):
+    n2 = cpf2 % 11
+    soma2 = soma2 + (n2 * i)
+    cpf2 =  cpf2 // 10
+
+soma2 = soma2 + (dv1 * 1)
+
+dv2 = soma2 % 11
+if dv2 >= 2:
+    dv2 = 11 - dv2
+else:
+    dv2 = 0
+
+print(f"O seus dois útilmos digitos verficadores são, repectivamente, {dv1} e {dv2}")
