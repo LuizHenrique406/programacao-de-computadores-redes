3b69010d56df4e0ba4fd938dcc92fc1de03042be
LuizHenrique406 <luiz.teixeira2803@gmail.com>
2026-05-19 10:41:58 -0300

    aulinha de for

--- atividades/project-euler-questoes/questao06-10/questao06.py
+++ atividades/project-euler-questoes/questao06-10/questao06.py
@@ -1,1 +1,12 @@
+print("Esse programa a diferença entre a soma dos quadrados dos primeiros cem números naturais e o quadrado da soma")
 
+soma100 = 0
+sq = 0
+for n in range(1,101):
+    soma100 = soma100 + n
+qs = soma100 ** 2
+
+for n in range(1,101):
+    sq = sq + n ** 2
+
+print(qs - sq)
