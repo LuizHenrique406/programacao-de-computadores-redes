f62bef9e24114c050ced5f8efe7893a2cf059c55
Luiz Henrique <luiz.teixeira2803@gmail.com>
2026-05-25 15:59:55 -0300

    feito em aula

--- materiais-de-aula/listas/lista.py
+++ materiais-de-aula/listas/lista.py
@@ -2,10 +2,9 @@
 # soma = sum(l)
 # print(soma)
 
-l = [3,4,9,12,8,15]
-soma = 0
-
+l = [3,4,9,23,12,8,15,20]
+maior = 0
 for e in l:
-    soma = soma + e
-
-print(soma)
+    if e > maior:
+        maior = e
+print(maior)
