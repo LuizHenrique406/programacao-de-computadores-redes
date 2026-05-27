dfd524497c73549a838ab652fa2137b6eb483b86
LuizHenrique406 <luiz.henriqueteixeira2008@gmail.com>
2026-05-27 18:30:35 -0300

    mano, como que automatiza isso, meu Deus? :(

--- atividades/exercícios/exercícios06/triangulo-pascal.py
+++ atividades/exercícios/exercícios06/triangulo-pascal.py
@@ -1,13 +1,28 @@
 print("Esse programa lhe apresenta o Triângulo de Pascal")
 
 e = [1,2,1]
-
-linha = 3
+g = e.copy()
+linha = 2
+print(e)
 
 x = 2
-e[x] = e[x] + e[x - 1]
+e[x] = g[x] + g[x - 1]
 x = 1
-e[x] = e[x] + e[x - 1]
+e[x] = g[x] + g[x - 1]
 e.append(1)
+linha = 3
+print(e)
+g = e.copy()
+
+x = 1
+e[x] = g[x] + g[x - 1]
+x = 2
+e[x] = g[x - 1] + g[x - 1]
+x = 3
+e[x] = g[x] + g[x - 1]
+e.append(1)
+linha = 4
+g = e.copy()
+
 print(e)
 
