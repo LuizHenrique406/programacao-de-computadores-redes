ff5d98a49953b4234bd3b1363b0ea8b27ae45997
Luiz Henrique <luiz.teixeira2803@gmail.com>
2026-06-01 15:54:52 -0300

    fazer depois

---
+++ atividades/exercícios/exercícios07/prática-lista.py
@@ -0,0 +1,4 @@
+str_uf        = 'Alagoas;Bahia;Ceará;Maranhão;Paraíba;Pernambuco;Piauí;Rio Grande do Norte;Sergipe'
+str_siglas    = 'AL;BA;CE;MA;PB;PE;PI;RN;SE'
+str_populacao = '3365351;14985284;9240580;7153262;4059905;9674793;3289290;3560903;2338474'
+
