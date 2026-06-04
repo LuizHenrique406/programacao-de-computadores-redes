5afa7ee0656953a4e4a3e3cbfe2650b6e9b26adf
LuizHenrique406 <luiz.teixeira2803@gmail.com>
2026-06-04 15:18:23 -0300

    a ideia tá certa, só falta ajeitar a lógica condicional

---
+++ atividades/listas/lista02/questao07/triangulo_tipo.py
@@ -0,0 +1,14 @@
+print("Esse programa indica qual é o tipo do triângulo a partir do números de lados")
+
+a = int(input("Qual o valor do lado a?: "))
+b = int(input("Qual o valor do lado b?: "))
+c = int(input("Qual o valor do lado c?: "))
+
+if a == b and b == c and c == a:
+    print("Esse triãngulo é Equilátero")
+
+if a != b or b != c:
+    print("Esse triângulo é Isósceles")
+
+if a != b and b != c and c != a:
+    print("Esse triângulo é Escaleno")
