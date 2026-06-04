213b1c571a3995e2978d2374df60bae7d1474b44
LuizHenrique406 <luiz.teixeira2803@gmail.com>
2026-06-04 13:11:33 -0300

    .

---
+++ atividades/listas/lista02/questao03/ordem_crescente.py
@@ -0,0 +1,7 @@
+print("Esse programa coloca 3 valores em Ordem Crescente")
+
+valor1 = int(input("Digite o primeiro valor: "))
+valor2 = int(input("Digite o segundo valor: "))
+valor3 = int(input("Digite o terceiro valor: "))
+
+valores = []
