bbe2b8418255e2d0495fbfc436e7491b639447d8
LuizHenrique406 <luiz.teixeira2803@gmail.com>
2026-06-10 10:43:53 -0300

    não foi feito em 8 linhas, mas tá funcionando ;)

--- atividades/exercícios/exercicio08/troco_lista.py
+++ atividades/exercícios/exercicio08/troco_lista.py
@@ -8,4 +8,4 @@ for i in cedulas:
     quant.append(x)
     troco = troco % i
 quant.append(troco)
-print(quant)
+print(f"Cédula de 200: {quant[0]}\nCédula de 100: {quant[1]}\nCédula de 50: {quant[2]}\nCédula de 20: {quant[3]}\nCédula de 10: {quant[4]}\nCédula de 5: {quant[5]}\nCédula de 2: {quant[6]}\nmoedas: {quant[7]}")
