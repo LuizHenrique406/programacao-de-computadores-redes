6fd48efa60a6b98d15dd7bdb9afabdc42b4fd1cd
Luiz Henrique <luiz.teixeira2803@gmail.com>
2026-06-12 13:46:46 +0000

    isso aqui é GOTY, slk ;)

--- praticas_aleatorias/listas/comprehensions.py
+++ praticas_aleatorias/listas/comprehensions.py
@@ -1,3 +1,8 @@
 a = [i for i in range(1,11) if i % 2 == 1]
 b = [i for i in range(1,11) if i % 2 == 0]
 print(f"Apenas ímapres menosres que 10: {a}\nApenas pares menores que 10: {b}")
+# isso aqui é de boa, piora quando é comparação ou fazer a partir de outra lista :(
+S = [ x**2 for x in range(20) ]
+K = [ x//1024 for x in S if x % 1024 == 0]
+print(f"{K}")
+# tipo essa aqui, mas essa aqui também tá de boa, deve ter outras piores
