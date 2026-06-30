63af6f4ab9ea976fd41fd8a2be162d3587184726
LuizHenrique406 <luiz.teixeira2803@gmail.com>
2026-06-30 15:18:35 -0300

    .

--- atividades/listas/lista06/questao04/fatorial_digitos.py
+++ atividades/listas/lista06/questao04/fatorial_digitos.py
@@ -1,7 +1,11 @@
 def fatorial(n):
+    soma = 1
     num = []
     for i in range(n + 1):
         num.append(i)
     num = sorted(num, reverse = True)
     num.remove(0)
-    
+    for i in num:
+        soma = soma * i
+    return soma
+def 
