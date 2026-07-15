add68d94dfab5aa790ac0b652d72bbfd52f6bb3f
LuizHenrique406 <luiz.teixeira2803@gmail.com>
2026-07-14 22:52:21 -0300

    quase acabando, tá muito ineficiente mas é o jeito, falta só essa parte dos refs que tá com erro e aprender a reiniciar o treco aí, slk

--- placas/proje_ler_temo.py
+++ placas/proje_ler_temo.py
@@ -3,7 +3,7 @@ import neopixel
 import time
 sensor_temp = machine.ADC(4)
 np = neopixel.NeoPixel(machine.Pin(7), 25)
-joystick_x  = machine.ADC(27) 
+joystick_x  = machine.ADC(27)
 joystick_y  = machine.ADC(26)
 def temp():
     fator_conversao = 3.3 / 65535 
@@ -17,34 +17,56 @@ def atualiza_leds():
     num = num[::-1]
     for i in num:
         np[i] = np[i - 1]
-def posi_x():
-    posicao_x = joystick_x.read_u16()
-    cofre = posicao_x
-    posicao_x = joystick_x.read_u16()
-    return cofre - posicao_x
+ref = [40,35,30,25]
+soma = 1
 leitura = 2
 while True:
     temperatura = temp()
-    if cofre - posicao_y > 2000:
-        leitura = leitura - 0.1
-    elif cofre - posicao_x < -2000:
-        leitura = leitura + 0.1
+    posicao_y = joystick_y.read_u16()
+    posicao_x = joystick_x.read_u16()
+    if posicao_y > 32000:
+        leitura -= 0.1
+    else:
+        leitura += 0. 
     if leitura > 2:
-        leitura = leitura - 0.1
+        leitura -= 0.1
     elif leitura < 0:
-        leitura = leitura + 0.1
-    if temperatura > 40 or temperatura == 40:
+        leitura += 0.1
+    if posicao_x > 35000:
+        ref = list(map(lambda x:x - soma, ref))
+        if ref[0] > 50:
+            ref[0] -= 1
+        if ref[1] > 40:
+            ref[1] -= 1
+        if ref[2] > 35:
+            ref[2] -= 1
+        if ref[3] > 30:
+            ref[3] -= 1    
+        print(ref)
+    if posicao_x < 30000:
+        ref = list(map(lambda x:x + soma, ref))
+        if ref[0] < 25:
+            ref[0] += 1
+        if ref[1] <35:
+            ref[1] += 1
+        if ref[2] < 30:
+            ref[2] += 1
+        if ref[3] < 25:
+            ref[3] += 1    
+        print(ref) 
+    if temperatura > ref[0] or temperatura == ref[0]:
         np[0] = (50,0,0)
-    elif temperatura > 35 and temperatura < 40:
+    elif temperatura > ref[1] and temperatura < ref[0]:
         np[0] = (50,30,0)
-    elif temperatura > 30 and temperatura < 35:
+    elif temperatura > ref[2] and temperatura < ref[1]:
         np[0] = (0,0,50)
-    elif temperatura > 25 and temperatura < 30:
+    elif temperatura > ref[3] and temperatura < ref[2]:
         np[0] = (0,50,0)
-    elif temperatura < 25:
+    elif temperatura < ref[3]:
         np[0] = (0,0,0)
     np.write()
     atualiza_leds()
-    print(leitura)
     print(temperatura)
+    print(leitura)
+    print(posicao_x)
     time.sleep(leitura)
