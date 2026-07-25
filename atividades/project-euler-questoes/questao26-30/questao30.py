776fabaf0f6557d9464c69320efe0d89f9cc5419
LuizHenrique406 <luiz.teixeira2803@gmail.com>
2026-07-25 13:36:33 -0300

    questao resolvida com sucesso

---
+++ atividades/project-euler-questoes/questao26-30/questao30.py
@@ -0,0 +1,16 @@
+print("Eesse programa ncontra a soma de todos os números que podem ser escritos como a soma das quintas potências de seus dígitos")
+def pot_dig(n):
+    nums = []
+    soma = 0
+    for i in str(n):
+        nums.append(i)
+    for i in nums:
+        soma += int(i) ** 5
+    if soma == n:
+        return soma
+    else:
+        return 0
+soma = 0
+for i in range(2, 194980):
+    soma += pot_dig(i)
+print(f"A soma desses números foi de {soma}")
